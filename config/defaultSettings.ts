4967a569f1fdc1cbe88ee18731a4cb771c08a90a
Zhang Shaocong <zhangshaocong@boe.com.cn>
2020-12-04 17:39:22 +0800

    test commit

--- config/defaultSettings.ts
+++ config/defaultSettings.ts
@@ -7,8 +7,8 @@ const Settings: LayoutSettings & {
   navTheme: 'light',
   // 拂晓蓝
   primaryColor: '#1890ff',
-  layout: 'mix',
-  contentWidth: 'Fluid',
+  layout: 'top',
+  contentWidth: 'Fixed',
   fixedHeader: false,
   fixSiderbar: true,
   colorWeak: false,
